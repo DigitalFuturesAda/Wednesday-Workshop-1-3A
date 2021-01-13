de0ebe7ca6b70667b965acc4c690e6b378de7500
Lyons <58537339+DigitalFuturesAda@users.noreply.github.com>
2021-01-13 14:23:45 +0000

    added more filess

---
+++ E2.cpp
@@ -0,0 +1,41 @@
+// https://repl.it/@B3GHU8/createthefile#main.cpp
+//re
+#include <iostream>
+#include <fstream> // imports libs
+
+bool is_file_exist(const char *fileName){
+    std::ifstream infile(fileName);
+    return infile.good(); // checks if a file exists based on a given filename
+}
+
+int main() {
+  std::ofstream myfile;
+
+  if(is_file_exist("list.csv")){ //check if file exists
+    myfile.open ("list.csv", std::fstream::in | std::fstream::out | std::fstream::app);// open file in append mode
+  } else {
+    myfile.open ("list.csv");
+  }
+
+  bool editting = true; // keep in while loop
+
+  do {
+    std::string userInputString;
+    std::cout << "Enter name: ";
+
+    getline(std::cin, userInputString);
+
+    myfile << userInputString << "\n";
+
+    std::string another;
+    std::cout << "Add another (y/n): "; // add names
+    getline(std::cin, another);
+
+    if (another == "n" || another == "n"){
+      editting = false; // exit
+    }
+
+  } while (editting);
+  myfile.close();
+  std::cout << "Good bye!";
+}
