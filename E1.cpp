ec61907a080a6843739e16497c92f5a89e630a4c
Lyons <58537339+DigitalFuturesAda@users.noreply.github.com>
2021-01-13 14:05:20 +0000

    added link to repl and add comments

--- E1.cpp
+++ E1.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
+// https://repl.it/@B3GHU8/doesfileexist#main.cpp
+// run the code here, this is a comment ^
+
+#include <iostream> // input output stream
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string>
-#include <fstream>
+#include <fstream> // import all the useful libraries
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/stat.h> // stat is needed to get stats about the file
 #include <filesystem>
 
 int main() {
   std::cout << "Does file exist: ";
-  std::string fileToGo; 
+  std::string fileToGo;  // define string
 
   getline(std::cin, fileToGo); //get input
 
@@ -17,9 +20,9 @@ int main() {
   if (stat(fileToGo.c_str(), &buffer) == 0) { // does the file exist?
     std::cout << fileToGo << ", " << buffer.st_size << " bytes - last modified at: " << ctime(&buffer.st_mtime) << std::endl;
     // print the stuff
-  } else {
+  } else { // create else
     std::cout << fileToGo << " does not exist." << std::endl; // print no it doesnt
-  }
+  } // end the else
 }
 
 // checks a file 
